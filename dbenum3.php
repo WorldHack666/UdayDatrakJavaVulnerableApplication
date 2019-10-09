c53c141fce73e02f046fdd9fb8d7b52e3bc53118
udaydatrak <theudaydatrak@gmail.com>
2019-10-09 21:13:51 +0530

    Add files via upload

---
+++ dbenum3.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+$result = mysqli_query($conn,"SHOW DATABASES"); while ($row = 
+mysqli_fetch_array($result)) { echo $row[0]."<br>"; }
+echo "Connected successfully";
+?>
